4ad614d562c53313e5162103e844b8526d243f0b
Noah Blumenthal <noah@statichippo.com>
2013-07-10 16:28:10 -0400

    npm install client before running

--- spawn.js
+++ spawn.js
@@ -1,4 +1,4 @@
-var spawn = require('child_process').spawn;
+var child_process = require('child_process');
 var path  = require('path');
 var redis = require('redis').createClient();
 var argv  = require('optimist')
@@ -9,6 +9,8 @@ var argv  = require('optimist')
   .default('spawn_ms', 50)
   .demand(['library'])
   .argv;
+
+require('posix').setrlimit('nofile', { soft: '9999', hard: '9999' });
 
 var children = [];
 var teardown = function() {
@@ -28,31 +30,38 @@ var delays_key = argv.library + ':delays';
 redis.del(disconnects_key);
 redis.del(delays_key);
 
-var client_path = path.join(path.dirname(process.argv[1]), argv.library, 'client.js');
-var i = 0;
-var spawnChild = function() {
-  redis.get(disconnects_key, function(err, disconnects) {
-    redis.llen(delays_key, function(err, delays) {
-      var exit = false;
-      if (disconnects && disconnects > argv.max_disconnects) {
-        exit = true;
-      }
-      if (delays && delays > argv.max_delays) {
-        exit = true;
-      }
-      if (exit) {
-        console.info(i + 'clients spawned');
-        console.info((disconnects || 0) + ' disconnects');
-        console.info((delays || 0) + ' delays (of >' + argv.delay_ms + 'ms)');
-        teardown();
-        return;
-      }
-      i = i+1;
-      var child = spawn('node', [client_path, '--id=' + i, '--delay_ms=' + argv.delay_ms]);
-      children.push(child);
-      setTimeout(spawnChild, argv.spawn_ms);
+var library_path = path.join(path.dirname(process.argv[1]), argv.library);
+
+var main = function() {
+  var client_path = path.join(library_path, 'client.js');
+  var i = 0;
+  var spawnChild = function() {
+    redis.get(disconnects_key, function(err, disconnects) {
+      redis.llen(delays_key, function(err, delays) {
+        var exit = false;
+        if (disconnects && disconnects > argv.max_disconnects) {
+          exit = true;
+        }
+        if (delays && delays > argv.max_delays) {
+          exit = true;
+        }
+        if (exit) {
+          console.info(i + 'clients spawned');
+          console.info((disconnects || 0) + ' disconnects');
+          console.info((delays || 0) + ' delays (of >' + argv.delay_ms + 'ms)');
+          teardown();
+          return;
+        }
+        i = i+1;
+        var child = child_process.spawn('node', [client_path, '--id=' + i, '--delay_ms=' + argv.delay_ms]);
+        children.push(child);
+        setTimeout(spawnChild, argv.spawn_ms);
+      });
     });
-  });
+  };
+
+  spawnChild();
 };
 
-spawnChild();
+// run npm install on the library directory then run the main function
+child_process.exec('npm install', { cwd: library_path }, main);
